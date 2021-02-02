25dd49250d0fe7653153bf8250e46aba4a97a5c5
hamzakoc <hamzakooc@gmail.com>
2021-02-02 11:31:17 -0500

    Single Page

---
+++ COMP2139-Lab2-TipCalculator/COMP2139-Lab2-TipCalculator/Models/TipCalculator.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Lab2_TipCalculator.Models
+{
+    public class TipCalculator
+    {
+
+        [Required(ErrorMessage = "Please enter a value for the cost of your meal.")]
+        [Range(0.0,10000.0,ErrorMessage="Cost of meal must be greater than zero")]
+        public double? MealCost { get; set; }
+
+        public double CalculateTip(double percent)
+        {
+
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * percent;
+                return tip;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+
+    }
+}
